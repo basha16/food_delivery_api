91c8cfc1ef070fa9c770aa052480e06053eb0ec3
ersathbasha <ersathbasha.a@gmail.com>
2024-03-27 21:59:50 +0530

    changes

--- src/controllers/delivery.js
+++ src/controllers/delivery.js
@@ -4,17 +4,14 @@ const { getPricingDetails } = require("../services/delivery");
 async function calculateDeliveryCost(param) {
   const { total_distance } = param;
   const pricingDetails = await getPricingDetails(param);
-  console.log(pricingDetails,'pricingDetails')
 
-  let total_price = pricingDetails.fix_price;
+  let total_price = parseInt(pricingDetails.fix_price);
   if (total_distance > pricingDetails.base_distance_in_km) {
     total_price +=
       (total_distance - pricingDetails.base_distance_in_km) *
-      pricingDetails.km_price *
-      100;
+      pricingDetails.km_price;
   }
-  let convertToEuros = (total_price * 100).toFixed(2);
-  console.log(total_price,'total_price')
+  let convertToEuros = total_price;
   return convertToEuros;
 }
 
